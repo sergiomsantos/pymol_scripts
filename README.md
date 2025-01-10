b170f156d84064b12509a48ee08978c4762592bf
Sérgio Santos <smf.dos.santos@gmail.com>
2025-01-10 17:28:06 +0000

    Add remote load script

---
+++ README.md
@@ -0,0 +1,38 @@
+# PyMOL utility scripts
+
+### Installation
+
+1. Download the selected script files to your local drive;
+2. Add the file to your `.pymolrc` for automatic loading
+   whenever a new instance of PyMOL is created.
+
+
+### Example
+
+The following example automatically loads the `loadr.py` script:
+
+```
+# file ~/.pymolrc
+
+run ~/path/to/loadr.py
+```
+
+To test, open a new instance of PyMOL and in the command prompt type:
+
+```
+PyMOL>help loadr
+
+DESCRIPTION
+
+    "loadr" load remote file.
+
+    "loadr" will attempt to load a remote file from the given host.
+    If the host is not provided, the action will fallback to loading
+    the file with the given name from the local drive.
+
+USAGE
+ 
+    loadr filename, [host=None [, *args [, **kwargs]]]
+
+    ()...)
+```
---
+++ loadr.py
@@ -0,0 +1,63 @@
+import subprocess
+import tempfile
+
+from pymol import cmd
+
+def loadr(filename, host=None, *args, **kwargs):
+    '''
+
+    DESCRIPTION
+
+        "loadr" load remote file.
+
+        "loadr" will attempt to load a remote file from the given host.
+        If the host is not provided, the action will fallback to loading
+        the file with the given name from the local drive.
+
+    USAGE
+
+        loadr filename, [host=None [, *args [, **kwargs]]]
+
+        Example:
+
+            PyMOL> loadr path/to/molecule.pdb, user@host
+
+    NOTE
+
+        This command assumes that iteraction with the remote host
+        if configured via SSH keys (typically via ~/.ssh/config;
+        remote files are copied via SCP).
+
+    ARGUMENTS
+
+        filename = string: file path or URL
+        host = host
+        *args, **kwargs = additional arguments to the "load" command
+
+    SEE ALSO
+
+        load
+
+        (c) Sergio M. Santos, University of Aveiro, 1st Feb. 2017
+
+    '''
+
+    if host:
+        fname = filename.split('/')[-1]
+        tmpdir = tempfile.gettempdir()
+        scp_source = host + ':' + filename
+        scp_target = tmpdir + '/' + fname
+        try:
+            subprocess.call(['scp', scp_source, scp_target])
+        except:
+            print(f'Cannot load remote file {scp_source}')
+            raise IOError
+        else:
+            cmd.load(scp_target, *args, **kwargs)
+            print(f'Loaded {scp_source}')
+    else:
+        cmd.load(filename, *args, **kwargs)
+
+    return
+
+cmd.extend('loadr', loadr)
